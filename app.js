7afb13acc57c0728b40ea049a6df219629b489e7
Ivan5 <converse_ivan@hotmail.com>
2018-03-18 20:13:45 -0600

    Display the Weather

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //Init Weather class
 const weather = new Weather('Boston','MA');
+//Init UI class
+const ui = new UI();
 //weather.changeLocation('Miami','FL');
 //Get weather on dom load
 document.addEventListener('DOMContentLoaded',getWeather);
 function getWeather(){
   weather.getWeather()
   .then(results => {
-    console.log(results);
+    ui.paint(results);
   })
   .catch(err => console.log(err));
 }
